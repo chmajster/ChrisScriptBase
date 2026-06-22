21113ba75d11d3fa7fcb1320bc295e15515c86ec
chmajster <chmajster@gmail.com>
2026-06-22 14:21:52 +0200

    Update diagnostic.sh

--- linux/diagnostic.sh
+++ linux/diagnostic.sh
@@ -7,6 +7,35 @@ UNAME=$(which uname)
 LSB_RELEASE=$(which lsb_release)
 GREP=$(which grep)
 
+if [[ -r /etc/SuSE-release ]]
+# /etc/os-release does not exist on SLES11 and RHEL6
+# so OS specific release files need to be used
+then
+    OSNAME=sles
+    OSVERS=$(awk '/^VERSION/ { printf("%s", $3) }' /etc/SuSE-release)
+    OS_SP=$(awk '/^PATCHLEVEL/ { printf("%d\n", $3) }' /etc/SuSE-release)
+    ZYPPER=$(which zypper)
+elif [[ -r /etc/redhat-release ]]
+then
+    OSNAME=rhel
+    OSVERS=$(awk -F 'release' '{print $2}' /etc/redhat-release | awk '{print $1}' | awk -F '.' '{print $1}')
+    OS_SP=$(awk -F 'release' '{print $2}' /etc/redhat-release | awk '{print $1}' | awk -F '.' '{print $2}')
+    YUM=$(which yum)
+    DNF=$(which dnf)
+    
+elif [[ -r /etc/os-release ]]
+then
+    OSNAME=$(awk -F '=' '/^ID=/ { print tolower($2) }' /etc/os-release | sed 's/"//g')
+    OSVERS="$(grep '^\(VERSION_ID\)' /etc/os-release | sed -n 's/.*"\([[:digit:]]\+\).*/\1/p')"
+    OS_SP="$(grep '^\(VERSION_ID\)' /etc/os-release | sed -n 's/.*\.\([[:digit:]]\+\).*/\1/p')"
+    if [[ "$OSNAME" == "ubuntu" || "$OSNAME" == "debian" ]]; then
+        APT=$(which apt)
+    fi
+else
+    OSNAME="unsupported_os"
+    OSVERS="unknown_ver"
+    OS_SP="unknown_sp"
+fi
 
 function print_memory_usage() {
     info "Printing memory usage"
@@ -62,6 +91,24 @@ function print_repository_list() {
 
 }
 
+function print_locked_packages() {
+    info "Printing locked packages"
+
+    if [[ "$PACKAGE_MANAGER" == "zypper" ]]; then
+        zypper locks
+    elif [[ "$PACKAGE_MANAGER" == "yum" ]]; then
+        yum versionlock list
+    elif [[ "$PACKAGE_MANAGER" == "dnf" ]]; then
+        dnf versionlock list
+    elif [[ "$PACKAGE_MANAGER" == "apt" ]]; then
+        apt-mark showhold
+    else
+        error "Unsupported package manager: ${PACKAGE_MANAGER}"
+        exit 1
+    fi
+
+}
+
 
 function main() {
     detect_os_and_package_manager
@@ -71,7 +118,9 @@ function main() {
     print_system_info
     print_hostname
     print_repository_list
-    
+    print_locked_packages
 }
 
+main
 
+if you type 1 run 
